5506b90f85a615c859d4c0262706dba5e7a6f2ee
PeterB24 <109487332+PeterB24@users.noreply.github.com>
2023-02-22 10:52:17 -0800

    Update SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     red = (255,0,0)
     black = (0,0,0)
     window_color = (200,200,200)
-    apple_image = pygame.image.load('SnakeGameApple.png')
+    apple_image = pygame.image.load('Apple_icon.png')
     clock=pygame.time.Clock() 
     
     snake_head = [250,250]
